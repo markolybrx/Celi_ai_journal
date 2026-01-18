bae8994e8f0c793e4db38ec61e983036129e3d73
markolybrx <132954069+markolybrx@users.noreply.github.com>
2026-01-19 00:57:10 +0800

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,14 +1,2 @@
-// KILL SWITCH: Forces the browser to discard old caches and use the network
-self.addEventListener('install', (event) => {
-    self.skipWaiting(); // Immediately replace the old Service Worker
-});
-
-self.addEventListener('activate', (event) => {
-    // Optional: Explicitly delete old caches to be 100% sure
-    event.waitUntil(
-        caches.keys().then((keyList) => {
-            return Promise.all(keyList.map((key) => caches.delete(key)));
-        })
-    );
-    self.clients.claim(); // Take control of the page immediately
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
