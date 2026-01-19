7ca9fa71c8cca0b6835b8df61aaadc6c99b3cc43
markolybrx <132954069+markolybrx@users.noreply.github.com>
2026-01-20 05:57:39 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# 1. Start the Celery Worker (The Chef)
-# --concurrency=1 limits it to 1 task at a time (Crucial for Free Tier)
-# The '&' at the end makes it run in the background
 celery -A celery_worker.celery_app worker --loglevel=info --concurrency=1 &
-
-# 2. Start the Web Server (The Waiter)
-# This runs in the foreground and keeps the Render service alive
 gunicorn app:app
