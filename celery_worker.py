b6eb4beda5e0661807028ac903bd56840749e5ac
markolybrx <132954069+markolybrx@users.noreply.github.com>
2026-01-20 05:58:24 +0800

    Update celery_worker.py

--- celery_worker.py
+++ celery_worker.py
@@ -1,7 +1,3 @@
 import os
-from app import celery_app, app
-
-if __name__ == '__main__':
-    with app.app_context():
-        celery_app.start()
-        
+from app import app, celery_app
+if __name__ == '__main__': celery_app.start()
